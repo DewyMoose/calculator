ee47c4c6cbb252f56f719feab217e98d287becf6
Brenden Dougherty <bdougherty89@yahoo.com>
2025-03-17 19:14:05 -0700

    created UI for calculator

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
-console.log("test");
+// let choice = prompt("would you like to add, subtract, multiply, or divide?");
+// let num1 = parseFloat(prompt("enter your first number"));
+// let num2 = parseFloat(prompt("enter your second number"));
+
+if (choice == "add") {
+  add(num1, num2);
+} else if (choice == "subtract") {
+  subtract(num1, num2);
+} else if (choice == "multiply") {
+  multiply(num1, num2);
+} else if (choice == "divide") {
+  divide(num1, num2);
+}
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function subtract(num1, num2) {
+  console.log(num1 - num2);
+}
+
+function multiply(num1, num2) {
+  console.log(num1 * num2);
+}
+
+function divide(num1, num2) {
+  console.log(num1 / num2);
+}
